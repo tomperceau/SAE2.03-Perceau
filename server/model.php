5d475657155c4264969d82baa478e7d67918b544
Tom Perceau <tom.perceau@etu.unilim.fr>
2025-04-03 10:38:00 +0200

    maj credentials bdd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "perceau1");
+define("DBLOGIN", "perceau1");
+define("DBPWD", "perceau1");
