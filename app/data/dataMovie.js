73a4e88c409e1c6fc188cb8c96f34a14a5a9908a
tomperceau <tom.perceau@etu.unilim.fr>
2025-04-03 14:21:27 +0200

    creation de Movies dans app

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -18,7 +18,7 @@ let DataMovie = {};
     // fetch permet d'envoyer une requête HTTP à l'URL spécifiée. 
     // L'URL est construite en concaténant HOST_URL à "/server/script.php?direction=" et la valeur de la variable dir. 
     // L'URL finale dépend de la valeur de HOST_URL et de dir.
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=read&movies");
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies");
     // answer est la réponse du serveur à la requête fetch.
     // On utilise ensuite la méthode json() pour extraire de cette réponse les données au format JSON.
     // Ces données (data) sont automatiquement converties en objet JavaScript.
