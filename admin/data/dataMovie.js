87c90969c0d250a81af43ddecd4c6822cda340c9
Perceau Tom <perceau1@mmi-dev.unilim.fr>
2025-04-04 20:44:49 +0200

    ITERATION 2 OK, ajout thebatman dans la BDD et dans l'iteration 1

--- admin/data/dataMovie.js
+++ admin/data/dataMovie.js
@@ -3,17 +3,6 @@ let HOST_URL = "https://mmi.unilim.fr/~perceau1/SAE2.03-Perceau";
 
 let DataMovie = {}; 
 
-// DataMovie.add = async function (fdata) {
-//     let config = {
-//       method: "POST",
-//     };
-//     let answer = await fetch(
-//       HOST_URL + "/server/script.php?todo=addMovie",
-//       config
-//     );
-//     let data = await answer.json();
-//     return data;
-//   };
 
 
   DataMovie.addMovie = async function (fdata) {
--- server/model.php
+++ server/model.php
@@ -55,27 +55,3 @@ function addMovie($name, $year, $length, $description, $director, $id_category, 
     
 }
 
-// function addMovie($titre, $real, $annee, $duree, $des, $cat, $img, $url, $age) {
-//     $cnx = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME, DBLOGIN, DBPWD);
-
-//     $sql = "REPLACE INTO Movie (name, director, year, length, description, id_category, image, trailer, min_age) 
-//             VALUES (:titre, :realisateur, :annee, :duree, :desc, :categorie, :image, :url, :age)";
-
-//     $stmt = $cnx->prepare($sql);
-
-//     $stmt->bindParam(':titre', $titre);
-//     $stmt->bindParam(':realisateur', $real);
-//     $stmt->bindParam(':annee', $annee);
-//     $stmt->bindParam(':duree', $duree);
-//     $stmt->bindParam(':desc', $des);
-//     $stmt->bindParam(':categorie', $cat);
-//     $stmt->bindParam(':image', $img);
-//     $stmt->bindParam(':url', $url);
-//     $stmt->bindParam(':age', $age);
-
-//     $stmt->execute();
-//     $res = $stmt->rowCount();
-//     return $res; // Retourne le nombre de lignes affectées par l'opération
-// }
-
-
