49c5535dbc8d827f22877e17245d786178fedd61
Perceau Tom <perceau1@mmi-dev.unilim.fr>
2025-04-07 14:44:09 +0200

    ite4 pas finis

--- app/component/MovieCategory/script.js
+++ app/component/MovieCategory/script.js
@@ -1,11 +1,16 @@
+
 let templateFile = await fetch("./component/MovieCategory/template.html");
 let template = await templateFile.text();
 
 let MovieCategory = {};
 
 MovieCategory.format = function () {
+  let html = template;
 
-  return template;
+  // Remplace {{Category}} par l'appel à la fonction JavaScript
+  html = html.replace("{{Category}}", "C.filterByCategory(this.value)");
+
+  return html;
 };
 
 export { MovieCategory };
